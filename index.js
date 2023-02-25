ca49d4d41a6475c5d87404ebeb4686a1140bc465
Nolan <bugygamer.mc@gmail.com>
2023-02-25 14:39:16 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+
+  .then((json) => renderBooks(json));
+  
   
 }
 
@@ -9,9 +14,11 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    
+    
   });
 }
-
+//renderBooks(json)
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
